ecb9b7a5bf6e99de723dd8f8019fe103209746dd
aavulasaikiran <80740812+aavulasaikiran@users.noreply.github.com>
2025-06-28 16:11:23 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
-require('dotenv').config();
-
 
 mongoose.set('strictQuery', false);
 
+const mongoURI = "mongodb+srv://saikiran:Mongodb@123@cluster0.zvle0xu.mongodb.net/blogapp?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose.connect(process.env.MONGO_URI || "mongodb://mongo:27017/Blog").then(()=>{
-    console.log("connected!");
-}).catch((err)=>{
-    console.log(err);
-})
+// Replace <your_password> with your actual MongoDB password
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("MongoDB connected successfully!");
+}).catch((err) => {
+    console.error("MongoDB connection error:", err);
+});
